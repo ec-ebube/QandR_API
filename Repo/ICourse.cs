6a8ade021835c5f0a11887ae3d5929698816ebbb
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-04-03 13:59:23 +0100

    4th april 2023

--- Repo/ICourse.cs
+++ Repo/ICourse.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using qandr_api.DTO;
 using qandr_api.Models;
 
 namespace qandr_api.Repo
@@ -10,8 +11,8 @@ namespace qandr_api.Repo
     {
         public Task<IEnumerable<Course>> GetCourse();
         public Task<Course> GetCourse(string id);
-        public Task<Course> UpdateCourse(String id, Course course);
+        public Task<Course> UpdateCourse(String id, Courses_DTO course);
         public void DeleteCourse(string id);
-        public Task<Course> CreateCourse(Course course);
+        public Task<Course> CreateCourse(Courses_DTO course);
     }
 }
